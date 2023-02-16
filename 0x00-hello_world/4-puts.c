5d467b28dfd18978278209386019db7bc0d7c4e7
Karim Abuzaid <eng.karimedhat@gmail.com>
2023-02-16 15:10:20 -0800

    puts task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - simple main function that prints msg to stdout
+ * Return: 0 exit code
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
