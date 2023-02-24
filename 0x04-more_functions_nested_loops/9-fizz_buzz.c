a32ce7493aa1c8468fa4587dccce9b9bb86cc500
Karim Abuzaid <eng.karimedhat@gmail.com>
2023-02-23 16:13:27 -0800

    9-fizz_buzz task

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print 0..100, with Fizz, Buzz for multiples of 3, 5
+ * Return: void
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		if (i % 5 == 0)
+			printf("Buzz");
+		if (i % 3 != 0 && i % 5 != 0)
+			printf("%d", i);
+
+		printf("%c", i < 100 ? ' ' : '\n');
+	}
+	return (0);
+}
