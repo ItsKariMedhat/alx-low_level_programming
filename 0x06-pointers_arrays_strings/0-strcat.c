48ee8686d4ae212d0aa6ad5ade448177fb73289b
Karim Abuzaid <eng.karimedhat@gmail.com>
2023-03-01 14:45:37 -0800

    adding 0-strcat task

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,13 +9,15 @@
  */
 char *_strcat(char *dest, const char *src)
 {
-	int index = 0, dest_len = 0;
+	char *p = dest;
 
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
-	return (dest);
+	/* move dest pointer to the end of the string */
+	for (; *dest; dest++)
+		{}
+	/* copy from src to dest */
+	for (; *src; src++, dest++)
+		*dest = *src;
+	/* set null char at the new end */
+	*dest = 0;
+	return (p);
 }
